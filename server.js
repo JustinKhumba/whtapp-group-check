aa0e7d609db469bf04410ea1e57ed36ae61632da
JustinKhumba <jmk1sttime@gmail.com>
2026-09-19 16:53:32 +0530

    dd

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
+// PATH: server.js
 const express = require("express");
 const path = require("path");
-
 const app = express();
 
+// Deployment Cost Limit: ₹0
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/user/:id", (req, res) => {
     const id = req.params.id;
-
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/check/:phone", (req, res) => {
     const phone = req.params.phone;
-
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
